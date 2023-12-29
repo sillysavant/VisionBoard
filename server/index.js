7252bb650eff0745bd893670f6581653caf63dd0
thithuthaonguyen <nguyent@allegheny.edu>
2023-12-28 19:15:39 -0500

    Server initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express  = require("express")
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello world'))
+
+const PORT = 3030
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
