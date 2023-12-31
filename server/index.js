5f6d95bd47a7664d55ca43f8093befb10b052885
thithuthaonguyen <nguyent@allegheny.edu>
2023-12-31 11:33:06 -0500

    Database connect

--- server/index.js
+++ server/index.js
@@ -1,9 +1,38 @@
-const express  = require("express")
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+// authRouter
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(
+			`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@vision-board.l5ztuzq.mongodb.net/?retryWrites=true&w=majority`,
+			{
+				useCreateIndex: true,
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useFindAndModify: false
+			}
+		)
+
+		console.log('MongoDB connected')
+	} catch (error) {
+		console.log(error.message)
+		process.exit(1)
+	}
+}
+
+connectDB()
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
-app.get('/', (req, res) => res.send('Hello world'))
+// app.use('/api/auth', authRouter)
+// app.use('/api/posts', postRouter)
 
-const PORT = 3030
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
